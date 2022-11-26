db5899ddbf57110960b42199dab0d2f2090af246
proflinervk <proflinervk@gmail.com>
2022-11-26 22:46:09 +0300

    create why-we section

--- src/js/mask.js
+++ src/js/mask.js
@@ -1,6 +1,6 @@
 import Inputmask from "inputmask";
 
-let selector = document.querySelector( '.input' )
+let selector = document.querySelectorAll( '.input' )
 
 let im = new Inputmask('+7 (999) 999-99-99')
 im.mask( selector )

ba9b6311d96b7604b7a0b85b4ded551b0361beda
proflinervk <proflinervk@gmail.com>
2022-12-02 14:56:45 +0300

    added new img and new pages

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,40 +1,45 @@
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
 import { setTargetElement, getTargetElement } from './common/global'
 
-document.addEventListener( 'DOMContentLoaded', () => {
-    'use strict'
 
-    showPopup()
+
+document.addEventListener( 'DOMContentLoaded', () => {
+	'use strict'
+
+	showPopup()
 } )
 
 const showPopup = () => {
-    const popupWrapper = document.querySelector( '.popup-wrapper' )
-    const closeButton = document.querySelector( '.popup-close' )
-    const body = document.querySelector( 'html')
-    setTargetElement( document.querySelector( '#body-lock' ) )
+	const popupWrapper = document.querySelector( '.popup-wrapper' )
+	const closeButton = document.querySelector( '.popup-close' )
+	const body = document.querySelector( 'header')
+	setTargetElement( document.querySelector( '#body-lock' ) )
 
-    body.addEventListener( 'mouseleave', () => {
+	body.addEventListener( 'mouseleave', e => {
+		const mouseY = e.clientY
 
-        if(  ! localStorage.getItem( 'showed' ) && ! popupWrapper.classList.contains( 'showed' ) ) {
-            // localStorage.setItem( 'showed', 1 )
-            popupWrapper.classList.add( 'showed' )
-            disableBodyScroll( getTargetElement() )
-        }
-    } )
+		if(  mouseY <= 0 ) {
+			if(  ! localStorage.getItem( 'showed' ) && ! popupWrapper.classList.contains( 'showed' ) ) {
+				// localStorage.setItem( 'showed', 1 )
+				popupWrapper.classList.add( 'showed' )
+				disableBodyScroll( getTargetElement(), { reserveScrollBarGap: true } )
+			}
+		}
+	} )
 
-    closeButton.addEventListener( 'click', () => {
-        popupWrapper.classList.remove( 'showed' )
-        enableBodyScroll( getTargetElement() )
-    } )
+	closeButton.addEventListener( 'click', () => {
+		popupWrapper.classList.remove( 'showed' )
+		enableBodyScroll( getTargetElement() )
+	} )
 
-    popupWrapper.addEventListener( 'click', e => {
-        e.stopPropagation()
+	popupWrapper.addEventListener( 'click', e => {
+		e.stopPropagation()
 
-        const target = e.target
+		const target = e.target
 
-        if ( target.className && target.classList.contains( 'popup-wrapper' ) ) {
+		if ( target.className && target.classList.contains( 'popup-wrapper' ) ) {
 			popupWrapper.classList.remove( 'showed' )
 			enableBodyScroll( getTargetElement() )
 		}
-    } )
+	} )
 }
